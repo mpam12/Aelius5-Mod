598890e78c205d2e0a7b7df6f3dc79d53474f5b3
mpam12 <malcolmpaulmcginnis@gmail.com>
2025-01-01 14:56:04 -0600

    adsfs

---
+++ src/main/java/net/aelius5/aelius5mod/Aelius5ModClient.java
@@ -0,0 +1,10 @@
+package net.aelius5.aelius5mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Aelius5ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/aelius5/aelius5mod/Aelius5ModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.aelius5.aelius5mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Aelius5ModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
